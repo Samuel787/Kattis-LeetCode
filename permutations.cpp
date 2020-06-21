510976873bcdd3cf8c825e75555589856839270c
samuel787 <suther.david.samuel@gmail.com>
2020-06-22 01:50:40 +0800

    documented permutation code

--- permutations.cpp
+++ permutations.cpp
@@ -4,12 +4,15 @@ using namespace std;
 void permutate(string input, int l, int r){
     //base case
     if(l == r){
+        //fixed all the letters from start to end (reached bottom of the recursion tree)
         cout << input << endl;
     } else {
+
+        //getting all the permutation of the string by swapping the first index with every index
         for(int i = l; i <= r; i++){
             swap(input[l], input[i]); //fixing the first index of the substring
-            permutate(input, l+1, r);
-            swap(input[l], input[i]);
+            permutate(input, l+1, r); //permutate the letters from l + 1 to r indices
+            swap(input[l], input[i]); //restore the original string
         }
     }
 }
