0857e804dc0b693c69e64fd8edc02773b81c59b6
samuel787 <suther.david.samuel@gmail.com>
2020-07-14 13:23:18 +0800

    tried to optimise with dp

--- fire2.cpp
+++ fire2.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 struct state{
-    vector<string> map;
     int steps;
     int width;
     int height;
@@ -18,11 +17,11 @@ struct state{
 /*
     Next state of the map with the @ at the same position
 */
-vector<string> next_map(state curr){
+vector<string> next_map(vector<string> map, int height, int width){
     char new_fire = '^';
-    vector<string> map = curr.map;
-    int height = curr.height;
-    int width = curr.width;
+    // vector<string> map = curr.map;
+    // int height = curr.height;
+    // int width = curr.width;
     for(int i = 0; i < height; i++){
         for(int j = 0; j < width; j++){
             if(map[i][j] == '*'){
@@ -61,10 +60,10 @@ vector<string> next_map(state curr){
 /*
     generates all the 4 neighbours
 */
-vector<state> generate_neighbours(state curr){
+vector<state> generate_neighbours(state curr, vector<string> new_map){
     //assert: @ is not at the border
     vector<state> new_states;
-    vector<string> new_map = next_map(curr);
+    //vector<string> new_map = next_map(curr);
     int new_x, new_y;
     
     //goes up
@@ -73,7 +72,7 @@ vector<state> generate_neighbours(state curr){
     if(new_map[new_y][new_x] != '#' && new_map[new_y][new_x] != '*'){
         vector<string> up_map = new_map;
         up_map[new_y][new_x] = '@';
-        new_states.push_back({up_map, curr.steps + 1, curr.width, curr.height, new_y, new_x});
+        new_states.push_back({curr.steps + 1, curr.width, curr.height, new_y, new_x});
     }
     
     //goes down
@@ -82,7 +81,7 @@ vector<state> generate_neighbours(state curr){
     if(new_map[new_y][new_x] != '#' && new_map[new_y][new_x] != '*' ){
         vector<string> up_map = new_map;
         up_map[new_y][new_x] = '@';
-        new_states.push_back({up_map, curr.steps + 1, curr.width, curr.height, new_y, new_x});
+        new_states.push_back({curr.steps + 1, curr.width, curr.height, new_y, new_x});
     }
     
     //goes left
@@ -91,7 +90,7 @@ vector<state> generate_neighbours(state curr){
     if(new_map[new_y][new_x] != '#' && new_map[new_y][new_x] != '*'){
         vector<string> up_map = new_map;
         up_map[new_y][new_x] = '@';
-        new_states.push_back({up_map, curr.steps + 1, curr.width, curr.height, new_y, new_x});
+        new_states.push_back({curr.steps + 1, curr.width, curr.height, new_y, new_x});
     }
     
     //goes right
@@ -100,7 +99,7 @@ vector<state> generate_neighbours(state curr){
     if(new_map[new_y][new_x] != '#' && new_map[new_y][new_x] != '*'){
         vector<string> up_map = new_map;
         up_map[new_y][new_x] = '@';
-        new_states.push_back({up_map, curr.steps + 1, curr.width, curr.height, new_y, new_x});
+        new_states.push_back({curr.steps + 1, curr.width, curr.height, new_y, new_x});
     }
     return new_states;
 }
@@ -136,13 +135,15 @@ int main(){
         cin >> h;
         int x, y;
         bool found = false;
+        vector<vector<string>> all_maps;
         vector<string> map(h, "");
         for(int i = 0; i < h; i++){
             cin >> map[i];
         }
+        all_maps.push_back(map);
         queue<state> q;
         pair<int, int> initial_pos = get_pos(map, w, h);
-        state initial = {map, 1, w, h, initial_pos.first, initial_pos.second};
+        state initial = {1, w, h, initial_pos.first, initial_pos.second};
         q.push(initial);
         set<pair<int, int>> visited;
         while(!q.empty() ){
@@ -157,7 +158,12 @@ int main(){
                 continue;
             }
             visited.insert(make_pair(curr.x, curr.y));
-            vector<state> new_states = generate_neighbours(curr);
+
+            vector<state> new_states;
+            if(curr.steps == all_maps.size()){
+                all_maps.push_back(next_map(all_maps[all_maps.size() - 1], h, w));
+            }
+            new_states = generate_neighbours(curr, all_maps[curr.steps]);
             for(int i = 0; i < new_states.size(); i++){
                 if(visited.find(make_pair(new_states[i].x, new_states[i].y)) == visited.end()){
                     q.push(new_states[i]);
@@ -169,3 +175,176 @@ int main(){
         }
     }
 }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// struct state{
+//     vector<string> map;
+//     int steps;
+//     int width;
+//     int height;
+//     int y;
+//     int x;
+
+//     bool operator <(const state & playerObj) const
+//     {
+//         return x < playerObj.x;
+//     }
+// };
+
+// /*
+//     Next state of the map with the @ at the same position
+// */
+// vector<string> next_map(state curr){
+//     char new_fire = '^';
+//     vector<string> map = curr.map;
+//     int height = curr.height;
+//     int width = curr.width;
+//     for(int i = 0; i < height; i++){
+//         for(int j = 0; j < width; j++){
+//             if(map[i][j] == '*'){
+//                 //spread the fire
+//                 //up
+//                 if(i-1 >= 0 && map[i-1][j] != '*' && map[i-1][j] != '#'){
+//                     map[i-1][j] = new_fire;
+//                 }
+//                 //down
+//                 if(i+1 < height && map[i+1][j] != '*' && map[i+1][j] != '#'){
+//                     map[i+1][j] = new_fire;
+//                 }
+//                 //left
+//                 if(j-1 >= 0 && map[i][j-1] != '*' && map[i][j-1] != '#'){
+//                     map[i][j-1] = new_fire;
+//                 }
+//                 //right
+//                 if(j+1 < width && map[i][j + 1] != '*' && map[i][j+1] != '#'){
+//                     map[i][j+1] = new_fire;
+//                 }
+//             }
+//         }
+//     }
+
+//     for(int i = 0; i < height; i++){
+//         for(int j = 0; j < width; j++){
+//             if(map[i][j] == '^'){
+//                 map[i][j] = '*';
+//             }
+//         }
+//     }
+
+//     return map;
+// }
+
+// /*
+//     generates all the 4 neighbours
+// */
+// vector<state> generate_neighbours(state curr){
+//     //assert: @ is not at the border
+//     vector<state> new_states;
+//     vector<string> new_map = next_map(curr);
+//     int new_x, new_y;
+    
+//     //goes up
+//     new_y = curr.y - 1;
+//     new_x = curr.x;
+//     if(new_map[new_y][new_x] != '#' && new_map[new_y][new_x] != '*'){
+//         vector<string> up_map = new_map;
+//         up_map[new_y][new_x] = '@';
+//         new_states.push_back({up_map, curr.steps + 1, curr.width, curr.height, new_y, new_x});
+//     }
+    
+//     //goes down
+//     new_y = curr.y + 1;
+//     new_x = curr.x;
+//     if(new_map[new_y][new_x] != '#' && new_map[new_y][new_x] != '*' ){
+//         vector<string> up_map = new_map;
+//         up_map[new_y][new_x] = '@';
+//         new_states.push_back({up_map, curr.steps + 1, curr.width, curr.height, new_y, new_x});
+//     }
+    
+//     //goes left
+//     new_y = curr.y;
+//     new_x = curr.x - 1;
+//     if(new_map[new_y][new_x] != '#' && new_map[new_y][new_x] != '*'){
+//         vector<string> up_map = new_map;
+//         up_map[new_y][new_x] = '@';
+//         new_states.push_back({up_map, curr.steps + 1, curr.width, curr.height, new_y, new_x});
+//     }
+    
+//     //goes right
+//     new_y = curr.y;
+//     new_x = curr.x + 1;
+//     if(new_map[new_y][new_x] != '#' && new_map[new_y][new_x] != '*'){
+//         vector<string> up_map = new_map;
+//         up_map[new_y][new_x] = '@';
+//         new_states.push_back({up_map, curr.steps + 1, curr.width, curr.height, new_y, new_x});
+//     }
+//     return new_states;
+// }
+
+// /**
+//  *  returns true when you have reached the border. false otherwise
+//  */
+// bool check_state(state curr){
+//     if(curr.y == 0 || curr.x == 0 || curr.y == curr.height - 1 || curr.x == curr.width - 1){
+//         return true;
+//     }
+//     return false;
+// }
+
+// pair<int, int> get_pos(vector<string> map, int width, int height){
+//     for(int i = 0; i < height; i++){
+//         for(int j = 0; j < width; j++){
+//             if(map[i][j] == '@'){
+//                 return make_pair(i, j);
+//             }
+//         }
+//     }
+//     return make_pair(-1, -1);
+// }
+
+// int main(){
+//     int N;
+//     cin >> N;
+//     while(N--){
+//         int w;
+//         int h;
+//         cin >> w;
+//         cin >> h;
+//         int x, y;
+//         bool found = false;
+//         vector<string> map(h, "");
+//         for(int i = 0; i < h; i++){
+//             cin >> map[i];
+//         }
+//         queue<state> q;
+//         pair<int, int> initial_pos = get_pos(map, w, h);
+//         state initial = {map, 1, w, h, initial_pos.first, initial_pos.second};
+//         q.push(initial);
+//         set<pair<int, int>> visited;
+//         while(!q.empty() ){
+//             state curr = q.front();
+//             if(check_state(curr)){
+//                 found = true;
+//                 cout << curr.steps << endl;
+//                 break;
+//             }
+//             q.pop();
+//             if(visited.find(make_pair(curr.x, curr.y)) != visited.end()){
+//                 continue;
+//             }
+//             visited.insert(make_pair(curr.x, curr.y));
+//             vector<state> new_states = generate_neighbours(curr);
+//             for(int i = 0; i < new_states.size(); i++){
+//                 if(visited.find(make_pair(new_states[i].x, new_states[i].y)) == visited.end()){
+//                     q.push(new_states[i]);
+//                 }
+//             }
+//         }
+//         if(!found){
+//             cout << "IMPOSSIBLE" << endl;
+//         }
+//     }
+// }
