e0b2e0990e0745ebea124641d46eef950a5f1da6
samuel787 <suther.david.samuel@gmail.com>
2020-08-02 01:19:55 +0800

    completed sum path on leetcode

---
+++ sum_path_leetcode.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(root == nullptr){
+            return false; // empty tree
+        }
+        
+        //leaf node
+        if(root -> left == nullptr && root -> right == nullptr){
+            if(root -> val - sum == 0){
+                return true;
+            } else {
+                return false; //backtrack
+            }
+        }
+        
+        if(hasPathSum(root -> left, sum - root -> val) || hasPathSum(root -> right, sum - root -> val)){
+            return true;
+        }
+        return false;
+    }
+};
