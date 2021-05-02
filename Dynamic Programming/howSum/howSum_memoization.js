68a91d9b1d2e55a61f5b6c17620db3fe694bc3f5
Samuel787 <suther.david.samuel@gmail.com>
2021-05-02 15:08:57 +0800

    [Dynamic Programming] HowSum Memoized

---
+++ Dynamic Programming/howSum/howSum_memoization.js
@@ -0,0 +1,26 @@
+
+function howSum(target, arr, memo={}){
+    if(target in memo){
+        return memo[target];
+    }
+    if(target === 0){
+        return [];
+    }
+    if(target < 0){
+        return null;
+    }
+    var result = []
+    for(var i = 0; i < arr.length; i++){
+        var temp = howSum(target - arr[i], arr, memo);
+        memo[target - arr[i]] = temp;
+        if(temp != null){
+            temp.push(arr[i]);
+            // memo[target] = temp;
+            return temp;
+        }
+    }
+    return null;
+}
+
+console.log("This is the result, ", howSum(7, [5,4,3]))
+console.log("This is the result, ", howSum(300, [7,14]))
