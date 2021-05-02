a4e3b6768cb47925982b14570295a4262e68dc42
Samuel787 <suther.david.samuel@gmail.com>
2021-05-02 11:45:18 +0800

    [Dynamic Programming] CalcSum Recursive

---
+++ Dynamic Programming/calcSum/calcSum_recursive.js
@@ -0,0 +1,35 @@
+const getArrMin = (arr) => {
+    if(arr.length == 0){
+        return null;
+    }
+    var min = Number.POSITIVE_INFINITY;
+    arr.forEach(element => {
+        if(element < min){
+            min = element
+        }
+    });
+    return min;
+}
+
+
+const calcSum = (target, arr, min_in_arr=null) => {
+    if(target == 0){
+        return true;
+    }
+    if(min_in_arr == null){
+        min_in_arr = getArrMin(arr);
+    }
+    if(target < min_in_arr){
+        return false;
+    }
+    for(var i = 0; i < arr.length; i++){
+        if(calcSum(target - arr[i], arr, min_in_arr)){
+            return true;
+        }
+    }
+    return false;
+}
+
+arr = [5,3,4,7];
+console.log(calcSum(7, arr));
+
