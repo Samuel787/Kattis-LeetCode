94331451bfd5cda04c9fc8713ba21d1ead6a9438
Samuel Suther David <suther.david.samuel@gmail.com>
2021-05-14 03:24:30 +0800

    [Leetcode][Algo]Path_with_maximum_gold (space optimization)

--- PathWithMaximumGold.cpp
+++ PathWithMaximumGold.cpp
@@ -10,15 +10,7 @@ public:
         for(int i = 0; i < length; i++){
             for(int j = 0; j < width; j++){
                 if(grid[i][j] > 0){
-                    vector<vector<bool>> track;
-                    for(int k = 0; k < length; k++){
-                        vector<bool> sub_track;
-                        for(int l = 0; l < width; l++){
-                            sub_track.push_back(false);
-                        }
-                        track.push_back(sub_track);
-                    }
-                    int curr_gold = find_max_gold(grid, i, j, length, width, track);
+                    int curr_gold = find_max_gold(grid, i, j, length, width);
                     max_gold = max(max_gold, curr_gold);
                 }
             }
@@ -26,18 +18,18 @@ public:
         return max_gold;
     }
     
-    int find_max_gold(vector<vector<int>>& map, int i, int j, int length, int width, vector<vector<bool>>& track){
-        if(i < 0 || j < 0 || i >= length || j >= width || map[i][j] == 0 || track[i][j]){
+    int find_max_gold(vector<vector<int>>& map, int i, int j, int length, int width){
+        if(i < 0 || j < 0 || i >= length || j >= width || map[i][j] == 0 || map[i][j] == -1){
             return 0;
         }
         int curr_gold = map[i][j];
-        track[i][j] = true;
-        int left = find_max_gold(map, i, j - 1, length, width, track);
-        int right = find_max_gold(map, i, j + 1, length, width, track);
-        int up = find_max_gold(map, i - 1, j, length, width, track);
-        int down = find_max_gold(map, i + 1, j, length, width, track);
+        map[i][j] = -1;
+        int left = find_max_gold(map, i, j - 1, length, width);
+        int right = find_max_gold(map, i, j + 1, length, width);
+        int up = find_max_gold(map, i - 1, j, length, width);
+        int down = find_max_gold(map, i + 1, j, length, width);
+        map[i][j] = curr_gold;
         curr_gold += max(max(left, right), max(up, down));
-        track[i][j] = false;
         return curr_gold;
     }
 };
