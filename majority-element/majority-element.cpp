f29571617ac043c1226dc95628537438ddb5d5a1
Samuel Suther David <suther.david.samuel@gmail.com>
2021-07-03 01:21:19 +0800

    Time: 16 ms (82.55%), Space: 19.6 MB (47.96%) - LeetHub

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> map;
+        int threshold = nums.size() / 2 + 1;
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]] += 1;
+            if (map[nums[i]] >= threshold) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
