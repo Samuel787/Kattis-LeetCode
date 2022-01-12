71d524d582e96c37c592e71fbc929a26a0efc14a
Samuel Suther David <suther.david.samuel@gmail.com>
2022-01-12 17:32:45 +0800

    Time: 85 ms (29.92%), Space: 21.8 MB (7.88%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -13,28 +13,15 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        path = []
-        self.searchForVal(root, p, path)
-        q_path = []
-        self.searchForVal(root, q, q_path)
-        q_set = set(q_path)
-        for i in reversed(path):
-            if i in q_set:
-                return TreeNode(i)
+        if root == p or root == q:
+            return root
+        if p.val > root.val and q.val < root.val:
+            return root
+        if p.val < root.val and q.val > root.val:
+            return root
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
         
-    def searchForVal(self, root, p, path):
-        if root == None:
-            return False
-        if root == p:
-            path.append(root.val)
-            return True
-        path.append(root.val)
-        leftVal = self.searchForVal(root.left, p, path)
-        if leftVal:
-            return leftVal
-        rightVal = self.searchForVal(root.right, p, path)
-        if rightVal:
-            return rightVal
-        path.pop()
-        return False
-            
+    
