55cee821b4d74fa0232ee2f0fb3dcdd262742d9b
Samuel Suther David <suther.david.samuel@gmail.com>
2022-04-11 22:28:05 +0800

    Time: 24 ms (92.21%), Space: 13.7 MB (80.74%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def calPoints(self, ops):
+        """
+        :type ops: List[str]
+        :rtype: int
+        """
+        scores = []
+        for op in ops:
+            if op == "+":
+                scores.append(scores[-1] + scores[-2])
+            elif op == "D":
+                scores.append(scores[-1] * 2)
+            elif op == "C":
+                scores.pop()
+            else:
+                scores.append(int(op))
+        
+        return sum(scores)
