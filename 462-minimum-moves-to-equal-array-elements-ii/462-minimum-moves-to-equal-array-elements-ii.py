824ca49deab5c286d9d195c81b0a10feb375fcfb
Samuel Suther David <suther.david.samuel@gmail.com>
2022-07-01 01:01:22 +0800

    Time: 66 ms (74.12%), Space: 14.7 MB (44.71%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -8,7 +8,12 @@ class Solution(object):
         median = 0
         size = len(nums)
         if size % 2 == 0:
-            median = (nums[size/2 - 1] + nums[size/2]) // 2
+            # median = (nums[size/2 - 1] + nums[size/2]) // 2
+            temp = (nums[size/2 - 1] + nums[size/2]) / 2
+            if temp % 1 == 0:
+                median = temp // 1
+            else:
+                median = temp + .5
         else:
             median = nums[size // 2]
         steps = 0
