429fcbf6ce67151106180024986e4b18dbf7e6ef
Samuel Suther David <samuel.sutherdavid@grabtaxi.com>
2023-04-28 00:27:01 +0800

    optimized code for bulb switcher

--- bulb_switcher.py
+++ bulb_switcher.py
@@ -1,8 +1,3 @@
 class Solution(object):
     def bulbSwitch(self, n):
-        count = 0
-        square = 1
-        while square <= n:
-            count += 1
-            square = (count + 1) * (count + 1)
-        return count
+        return int(sqrt(n))
