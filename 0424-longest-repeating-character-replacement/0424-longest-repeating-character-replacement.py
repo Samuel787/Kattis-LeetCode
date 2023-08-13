52578970a3c15605e01214401fed565e2cb4b3dc
Samuel Suther David <suther.david.samuel@gmail.com>
2023-08-14 00:28:21 +0800

    Time: 106 ms (40.78%), Space: 13.7 MB (48.43%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,49 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        left = 0
-        right = left
-        maxLength = 0
-        lastIndex = len(s) - 1
-        lengthOfS = len(s)
-        counts = {}
-        while left < len(s) and right < len(s):
-            newChar = s[right]
-            if newChar in counts:
-                counts[newChar] += 1
-            else:
-                counts[newChar] = 1
+        count = {}
+        res = 0
+
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
             
-            maxCount = self.getMaxCountInDict(counts)
-
-            currWindowLength = right - left + 1
-            if currWindowLength - maxCount <= k:
-                # print("left: " + str(left) + " right: " + str(right) + " currWindowLength: " + str(currWindowLength) + " maxCount: " + str(maxCount))
-                maxLength = max(maxLength, currWindowLength)
-            else:
-                # print("left: " + str(left) + " right: " + str(right) + " currWindowLength: " + str(currWindowLength) + " maxCount: " + str(maxCount))
-                counts[s[left]] -= 1
-                left += 1
-                if left < lastIndex:
-                    currWindowLength -= 1
-                    newMaxCount = self.getMaxCountInDict(counts)
-                    if currWindowLength - newMaxCount <= k:
-                        maxLength = max(maxLength, currWindowLength)
-            right += 1
-
-        while left < len(s):
-            counts[s[left]] -= 1
-            left += 1
-            maxCount = self.getMaxCountInDict(counts)
-
-            currWindowLength = right - left + 1
-            if currWindowLength - maxCount <= k:
-                maxLength = max(maxLength, currWindowLength)
-        return maxLength
-
-    
-    def getMaxCountInDict(self, mDict):
-        currMax = 0
-        for key in mDict:
-            currMax = max(currMax, mDict[key])
-        return currMax
+            res = max(res, r - l + 1)
+        return res
