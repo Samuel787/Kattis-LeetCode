7cd666b0a1b1eb263fc34bd4763b91dd93147fac
Samuel Suther David <suther.david.samuel@gmail.com>
2023-10-02 08:05:21 +0800

    Time: 420 ms (80.3%), Space: 29.3 MB (9.27%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        cache = set()
+        for num in nums:
+            if num in cache:
+                return True
+            cache.add(num)
+        return False
