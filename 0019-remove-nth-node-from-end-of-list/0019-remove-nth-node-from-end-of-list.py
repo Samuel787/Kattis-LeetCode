5511f4177795a169e2268c15090a6fe043f2f8c6
Samuel Suther David <suther.david.samuel@gmail.com>
2023-10-14 01:20:01 +0800

    Time: 13 ms (77.51%), Space: 13.1 MB (94.71%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -10,28 +10,56 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        length = self.getListLength(head)
-        nodeIdx = length - n
-
         dummyHead = ListNode(-1, head)
+        start = 0
+        end = 0
+        
         prev = dummyHead
-        curr = head
-        count = 0
-        while count <= nodeIdx:
-            if count == nodeIdx:
-                prev.next = curr.next
-            else:
-                prev = curr
-                curr = curr.next
-            count += 1
+        curr = dummyHead.next
+
+        prevNodeRef = dummyHead
+        currNodeRef = dummyHead.next
+
+        while curr != None:
+            elementsInWindow = end - start + 1
+            if elementsInWindow == n + 1:
+                # increment the start pointer also
+                prevNodeRef = currNodeRef
+                currNodeRef = currNodeRef.next
+                start += 1
+            end += 1
+            print("Start value is " + str(start))
+            print("ref pointer val: " + str(currNodeRef.val))
+                
+            curr = curr.next
+
+        # remove the nth node from end
+        prevNodeRef.next = currNodeRef.next
         return dummyHead.next
+
+    # def removeNthFromEnd(self, head, n):
+    #     length = self.getListLength(head)
+    #     nodeIdx = length - n
+
+    #     dummyHead = ListNode(-1, head)
+    #     prev = dummyHead
+    #     curr = head
+    #     count = 0
+    #     while count <= nodeIdx:
+    #         if count == nodeIdx:
+    #             prev.next = curr.next
+    #         else:
+    #             prev = curr
+    #             curr = curr.next
+    #         count += 1
+    #     return dummyHead.next
                 
     
-    def getListLength(self, head):
-        length = 0
-        curr = head
-        while curr != None:
-            length += 1
-            curr = curr.next
-        return length
+    # def getListLength(self, head):
+    #     length = 0
+    #     curr = head
+    #     while curr != None:
+    #         length += 1
+    #         curr = curr.next
+    #     return length
         
