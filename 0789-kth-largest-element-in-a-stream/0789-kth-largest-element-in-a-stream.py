c33f02502e69c2d7c841c8b4f7eaa6588a85a7b1
Samuel Suther David <suther.david.samuel@gmail.com>
2023-10-23 23:38:27 +0800

    Time: 147 ms (18.28%), Space: 17.4 MB (98.52%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -7,23 +7,43 @@ class KthLargest(object):
         :type k: int
         :type nums: List[int]
         """
-        import heapq
         self.k = k
-        self.minHeap = nums
-        heapq.heapify(self.minHeap)
-        while len(self.minHeap) > k:
-            heapq.heappop(self.minHeap)
-        
+        self.window = []
+        for num in nums:
+            self.binaryInsert(num)
 
     def add(self, val):
         """
         :type val: int
         :rtype: int
         """
-        heapq.heappush(self.minHeap, val)
-        if len(self.minHeap) > self.k:
-            heapq.heappop(self.minHeap)
-        return self.minHeap[0]
+        self.binaryInsert(val)
+        return self.window[0]
+
+    def binaryInsert(self, num):
+        if len(self.window) == 0:
+            self.window.append(num)
+        elif num <= self.window[0]:
+            self.window.insert(0, num)
+        elif num > self.window[-1]:
+            self.window.append(num)
+        else:
+            # Assert num belongs in index 0 and N exclusive
+            left = 0
+            right = len(self.window) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                # print("this is the window: " + str(self.window) + ' and this is the mid: ' + str(mid))
+                if self.window[mid] <= num and self.window[mid + 1] >= num:
+                    self.window.insert(mid + 1, num)
+                    break
+                elif self.window[mid] > num:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+        while len(self.window) > self.k:
+            self.window.pop(0)
         
 
 
