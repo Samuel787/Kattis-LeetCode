1db2b80a95bff2a6aac019485707260d29517214
Samuel Suther David <suther.david.samuel@gmail.com>
2023-10-23 23:45:40 +0800

    Time: 89 ms (49.49%), Space: 17.6 MB (72.21%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,49 +1,25 @@
-
-
 class KthLargest(object):
-
     def __init__(self, k, nums):
         """
         :type k: int
         :type nums: List[int]
         """
         self.k = k
-        self.window = []
-        for num in nums:
-            self.binaryInsert(num)
+        self.minheap = nums
+        heapq.heapify(self.minheap)
+        while len(self.minheap) > k:
+            heapq.heappop(self.minheap)
+
 
     def add(self, val):
         """
         :type val: int
         :rtype: int
         """
-        self.binaryInsert(val)
-        return self.window[0]
-
-    def binaryInsert(self, num):
-        if len(self.window) == 0:
-            self.window.append(num)
-        elif num <= self.window[0]:
-            self.window.insert(0, num)
-        elif num > self.window[-1]:
-            self.window.append(num)
-        else:
-            # Assert num belongs in index 0 and N exclusive
-            left = 0
-            right = len(self.window) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                # print("this is the window: " + str(self.window) + ' and this is the mid: ' + str(mid))
-                if self.window[mid] <= num and self.window[mid + 1] >= num:
-                    self.window.insert(mid + 1, num)
-                    break
-                elif self.window[mid] > num:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-
-        while len(self.window) > self.k:
-            self.window.pop(0)
+        heapq.heappush(self.minheap, val)
+        if len(self.minheap) > self.k:
+            heapq.heappop(self.minheap)
+        return self.minheap[0]
         
 
 
