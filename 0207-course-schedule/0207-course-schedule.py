6b6c90b83580ddcdf43fe21d1b5e8dc323156ad3
Samuel Suther David <suther.david.samuel@gmail.com>
2024-07-24 09:54:14 +0800

    Time: 66 ms (63.57%), Space: 14.6 MB (43.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -7,10 +7,10 @@ class Solution(object):
         """
         self.graph = {}
         for course in range(numCourses):
-            self.graph[course] = []
+            self.graph[course] = set()
         
-        for coursePair in prerequisites:
-            self.graph[coursePair[0]].append(coursePair[1])
+        for prereq in prerequisites:
+            self.graph[prereq[0]].add(prereq[1])
 
         self.visited = set()
         self.cycle = set()
@@ -26,8 +26,8 @@ class Solution(object):
             return False
 
         self.cycle.add(course)
-        for prereq in self.graph[course]:
-            if self.dfs(prereq) == False:
+        for prereqCourse in self.graph[course]:
+            if self.dfs(prereqCourse) == False:
                 return False
         self.cycle.remove(course)
         self.visited.add(course)
