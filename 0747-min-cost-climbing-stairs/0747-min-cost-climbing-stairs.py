441b8591ff558354285575e8e52b5ee4af5c8773
Samuel Suther David <suther.david.samuel@gmail.com>
2024-07-29 23:46:16 +0800

    Time: 31 ms (80.3%), Space: 11.8 MB (21.27%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -4,21 +4,7 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        costArr = [-1 for i in cost]
-    
-        if len(cost) == 1:
-            return cost[0]
-        if len(cost) == 2:
-            return min(cost[0], cost[1])
-
-        costArr[0] = 0
-        costArr[1] = 0
-
-        for i in range(len(cost)):
-            if i < 2:
-                continue
-            costArr[i] = min(costArr[i - 1] + cost[i - 1], costArr[i - 2] + cost[i - 2])
-            print("cost to come to step " + str(i) + " costs " + str(costArr[i]))
-        
-        return min(costArr[-1] + cost[-1], costArr[-2] + cost[-2])
-            
+        totalCost = [0 for i in range(len(cost) + 1)]
+        for i in range(2, len(cost) + 1):
+            totalCost[i] = min(cost[i - 1] + totalCost[i - 1], cost[i - 2] + totalCost[i - 2])
+        return totalCost[-1]
