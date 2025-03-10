5b59cd58296b959e29e87b716a887404009e9642
Samuel Suther David <suther.david.samuel@gmail.com>
2025-03-10 23:58:26 +0800

    Time: 8676 ms (9.52%), Space: 12.5 MB (18.98%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -5,51 +5,42 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        height = len(board)
-        width = len(board[0])
-
-        for y in range(height):
-            for x in range(width):
-                if self.searchString(board, x, y, word):
+        self.path = set()
+        self.board = board
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if self.dfs(row, col, word):
                     return True
         return False
 
     
-    def searchString(self, board, x, y, substring):
-        if len(substring) == 0:
+    def dfs(self, row, col, word):
+        if len(word) == 0:
             return True
-        
-        if x < 0 or x >= len(board[0]) or y < 0 or y >= len(board):
+        if (row, col) in self.path:
             return False
-
-        if board[y][x] != substring[0]:
+        if row < 0 or col < 0 or row >= len(self.board) or col >= len(self.board[0]):
             return False
-        
-        board[y][x] = "-1"
-        newString = substring[1:] 
-        res = self.searchString(board, x - 1, y, newString) or self.searchString(board, x + 1, y, newString) or self.searchString(board, x, y - 1, newString) or self.searchString(board, x, y + 1, newString)
-        board[y][x] = substring[0]
-        return res
-        # print("x: " + str(x) + " y: " + str(y) + "current string :=> " +  substring)
-        
-        # # left
-        # if x > 0:
-        #     if self.searchString(board, x - 1, y, newString):
-        #         return True
-        # # right
-        # if x + 1 < len(board[0]):
-        #     if self.searchString(board, x + 1, y, newString):
-        #         return True 
-        # # up
-        # if y > 0:
-        #     if self.searchString(board, x, y - 1, newString):
-        #         return True
-        # # down
-        # if y + 1 < len(board):
-        #     if self.searchString(board, x, y + 1, newString):
-        #         return True
+        if word[0] != self.board[row][col]:
+            return False
+        self.path.add((row, col))
+        # check left
+        left = self.dfs(row, col - 1, word[1:])
+        if left:
+            return True
+        # check up
+        up = self.dfs(row - 1, col, word[1:])
+        if up:
+            return True
+        # check down
+        down = self.dfs(row + 1, col, word[1:])
+        if down:
+            return True
+        # check right
+        right = self.dfs(row, col + 1, word[1:])
+        if right:
+            return True
+        self.path.remove((row, col))
+        return False
 
-        
-
-        # return False
-        
+                
