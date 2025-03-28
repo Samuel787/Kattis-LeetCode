ad62b46a54bc05272977b3a845e57e798a0f4d37
Samuel Suther David <suther.david.samuel@gmail.com>
2025-03-28 11:11:50 +0530

    Time: 3 ms (70.04%), Space: 17 MB (75.41%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -7,28 +7,41 @@
 class Solution(object):
     def isValidBST(self, root):
         """
-        :type root: TreeNode
+        :type root: Optional[TreeNode]
         :rtype: bool
         """
         if root == None:
             return True
-        return self.isValidBSTHelper(root.left, None, root.val) and self.isValidBSTHelper(root.right, root.val, None)
+        # return self.helper(root.left, -2 ** 31 - 1 , root.val) and self.helper(root.right, root.val, -2 ** 31)
+        return self.helper(root, -2 ** 31 - 1, 2 ** 31)
+        
+        # return self.isValidSubtree(root, None)
 
-    
-    def isValidBSTHelper(self, root, lowerBound, upperBound):
+    def helper(self, root, low, high):
         if root == None:
             return True
         
-        if lowerBound != None and root.val <= lowerBound:
+        if root.val <= low or root.val >= high:
             return False
-        if upperBound != None and root.val >= upperBound:
-            return False
-
+        
         # check left
-        isLeftValid = self.isValidBSTHelper(root.left, lowerBound, root.val)
+        return self.helper(root.left, low, root.val) and self.helper(root.right, root.val, high)
 
-        # check right
-        isRightValid = self.isValidBSTHelper(root.right, root.val, upperBound)
-
-        return isLeftValid and isRightValid
+    def isValidSubtree(self, root, parent):
+        if root == None:
+            return True
+        if root.left != None:
+            if root.left.val >= root.val:
+                return False
+            if parent != None and root.left.val <= parent.val:
+                return False
+        
+        if root.right != None:
+            if root.right.val <= root.val:
+                return False
+            if parent != None and root.right.val >= parent.val:
+                return False
+        
+        return self.isValidSubtree(root.left, root) and self.isValidSubtree(root.right, root)
             
+        
