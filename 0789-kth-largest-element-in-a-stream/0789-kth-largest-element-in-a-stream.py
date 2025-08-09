b422343b73916a2fb3d15d5b50bd7e111b99b4b5
Samuel Suther David <suther.david.samuel@gmail.com>
2025-08-09 04:11:05 -0400

    Time: 63 ms (51.1%), Space: 18.3 MB (18.27%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,39 +1,31 @@
 class KthLargest(object):
+
     def __init__(self, k, nums):
         """
         :type k: int
         :type nums: List[int]
         """
-        self.maxHeap = [-1 * num for num in nums]
+        self.min_heap = []
         self.k = k
-        heapq.heapify(self.maxHeap)
-        self.minHeap = []
-
-        # we maintain size of minHeap as k - 1. So Kth Largest element can be found in the maxHeap
-        for i in range(k - 1):
-            res = heapq.heappop(self.maxHeap)
-            heapq.heappush(self.minHeap, res * -1)
-
-
+        for num in nums:
+            heapq.heappush(self.min_heap, num)
+        
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
+        
     def add(self, val):
         """
         :type val: int
         :rtype: int
-        """
-        if len(self.minHeap) == 0 and self.k == 1:
-            heapq.heappush(self.maxHeap, val * -1)    
-        else:
-            topOfMinHeap = heapq.heappop(self.minHeap)
-            if val > topOfMinHeap:
-                heapq.heappush(self.maxHeap, topOfMinHeap * -1)
-                heapq.heappush(self.minHeap, val)   
-            else:
-                heapq.heappush(self.maxHeap, val * -1)
-                heapq.heappush(self.minHeap, topOfMinHeap)
+        """ 
+        if len(self.min_heap) >= self.k and self.min_heap[0] >= val:
+                return self.min_heap[0]
+        heapq.heappush(self.min_heap, val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
         
-        return self.maxHeap[0] * -1
         
-
 
 
 # Your KthLargest object will be instantiated and called as such:
