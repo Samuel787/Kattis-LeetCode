affb367bfe4e287db57d3965d3ee0d6ad5a90c23
Samuel Suther David <suther.david.samuel@gmail.com>
2025-08-09 01:32:22 -0400

    Time: 0 ms (100%), Space: 12.5 MB (53.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,17 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        self.dp = [0 for i in range(n + 1)]
-        if n > 0:
-            self.dp[1] = 1
-        if n > 1:
-            self.dp[2] = 2
-        return self.computeWays(n) 
-    
-    def computeWays(self, n):
-        if n == 0:
-            return 0
-        if self.dp[n] != 0:
-            return self.dp[n]
-        self.dp[n] = self.computeWays(n - 1) + self.computeWays(n - 2)
-        return self.dp[n]
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        count = 0
+        cache = [0 for i in range(n + 1)]
+        cache[0] = 0
+        cache[1] = 1
+        cache[2] = 2
+        for i in range(3, n + 1):
+            cache[i] = cache[i - 1] + cache[i - 2]
+        return cache[n]
+        
