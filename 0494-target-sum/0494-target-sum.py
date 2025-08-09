abc0a0b5b341a5c330d7ac993f7ecabc9958a7a8
Samuel Suther David <suther.david.samuel@gmail.com>
2025-08-09 03:25:01 -0400

    Time: 191 ms (54.83%), Space: 13.1 MB (47.69%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -5,21 +5,37 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        self.cache = {}
-        self.target = target
-        self.nums = nums
-        return self.backtrack(0, 0)
+        table = [{} for _ in range(len(nums) + 1)]
+        table[0][0] = 1 # there is 1 way to get sum of 0 with 0 elements
 
-    def backtrack(self, idx, curr_sum):
-        if (idx, curr_sum) in self.cache:
-            return self.cache[idx, curr_sum]
+        # for i in range(len(nums)):
+        #     for curr_sum in table[i]:
+        #         count = table[i][curr_sum]
 
-        if idx == len(self.nums):
-            if curr_sum == self.target:
-                return 1
-            else:
-                return 0
-        
-        
-        self.cache[(idx, curr_sum)] = self.backtrack(idx + 1, curr_sum + self.nums[idx]) + self.backtrack(idx + 1, curr_sum - self.nums[idx])
-        return self.cache[(idx, curr_sum)]
+        #         positiveSum = curr_sum + nums[i]
+        #         negativeSum = curr_sum - nums[i]
+            
+        #         if positiveSum not in table[i + 1]:
+        #             table[i + 1][positiveSum] = 0
+        #         if negativeSum not in table[i + 1]:
+        #             table[i + 1][negativeSum] = 0
+                
+        #         table[i + 1][positiveSum] += count
+        #         table[i + 1][negativeSum] += count
+
+        # return table[len(nums)][target]
+        # i coded pethatically
+        for i in range(0, len(nums)):
+            row = table[i]
+            for key in row:
+                if (key - nums[i]) not in table[i + 1]:
+                    table[i + 1][key - nums[i]] = 0
+                table[i + 1][key - nums[i]] += table[i][key]
+            
+                if (key + nums[i]) not in table[i + 1]:
+                    table[i + 1][key + nums[i]] = 0
+                table[i + 1][key + nums[i]] += table[i][key]
+        if target not in table[len(nums)]:
+            return 0
+        return table[len(nums)][target]
+                
