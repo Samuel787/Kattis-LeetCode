264285db6ff58a5f5c8d83549f061cf884a9d594
Samuel Suther David <suther.david.samuel@gmail.com>
2025-08-24 12:24:20 -0400

    Time: 91 ms (30.34%), Space: 16 MB (8.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -7,23 +7,24 @@ class Solution(object):
         """
         rows = len(word1)
         cols = len(word2)
-
         dp = [[0 for _ in range(cols + 1)] for _ in range(rows + 1)]
 
-        # we initialize the base cases
-        for row in range(rows + 1):
-            dp[row][cols] = rows - row
-        for col in range(cols + 1):
-            dp[row][col] = cols - col
+        # we fill the outer bounds first
+        for i in range(rows + 1):
+            dp[i][0] = i
         
-        for row in range(rows - 1, -1, -1):
-            for col in range(cols - 1, - 1, -1):
-                if word1[row] == word2[col]:
-                    dp[row][col] = dp[row + 1][col + 1]
+        for i in range(cols + 1):
+            dp[0][i] = i
+        
+        # build up the dp arr
+        for i in range(1, rows + 1):
+            for j in range(1, cols + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    insertCount = dp[row][col + 1] + 1
-                    deleteCount = dp[row + 1][col] + 1
-                    replaceCount = dp[row + 1][col + 1] + 1
-                    dp[row][col] = min(insertCount, deleteCount, replaceCount)
-        
-        return dp[0][0]
+                    dp[i][j] = min(
+                        dp[i - 1][j],
+                        dp[i][j - 1],
+                        dp[i - 1][j - 1]
+                    ) + 1
+        return dp[rows][cols]
