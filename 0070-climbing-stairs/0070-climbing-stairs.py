25200a3ff113f8f55e37d09c279d9e2425f80607
Samuel Suther David <suther.david.samuel@gmail.com>
2025-09-09 09:20:27 -0400

    Time: 0 ms (100%), Space: 12.5 MB (54.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,16 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        count = 0
-        cache = [0 for i in range(n + 1)]
-        cache[0] = 0
-        cache[1] = 1
-        cache[2] = 2
-        for i in range(3, n + 1):
-            cache[i] = cache[i - 1] + cache[i - 2]
-        return cache[n]
+        dp = []
+        dp.append(1)
+        dp.append(2)
+        if n <= 2:
+            return dp[n-1]
+        for i in range(2, n):
+            dp.append(dp[i - 1] + dp[i - 2])
+        return dp[-1]
         
