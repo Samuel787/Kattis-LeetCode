4b10ebc6c86edf476bbd3967421785f1a13dd218
Samuel Suther David <suther.david.samuel@gmail.com>
2025-09-28 10:46:20 -0400

    Time: 28 ms (93.21%), Space: 19.7 MB (15.76%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -10,15 +10,12 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        if head == None or head.next == None:
-            return False
         slow = head
-        fast = head.next.next
-        while slow != None and fast != None:
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
             if slow == fast:
                 return True
-            slow = slow.next
-            if fast.next == None:
-                return False
-            fast = fast.next.next
         return False
+        
