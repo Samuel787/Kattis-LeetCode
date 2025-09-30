93e7f1bccf6a0dfd2397cad9a33997122bad9b23
Samuel Suther David <suther.david.samuel@gmail.com>
2025-09-30 02:20:31 -0400

    Time: 91 ms (30.75%), Space: 15.9 MB (31.81%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -5,26 +5,18 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
-        rows = len(word1)
-        cols = len(word2)
-        dp = [[0 for _ in range(cols + 1)] for _ in range(rows + 1)]
+        rows = len(word1) + 1
+        cols = len(word2) + 1
+        dp = [[0] * cols for _ in range(rows)]
 
-        # we fill the outer bounds first
-        for i in range(rows + 1):
-            dp[i][0] = i
-        
-        for i in range(cols + 1):
-            dp[0][i] = i
-        
-        # build up the dp arr
-        for i in range(1, rows + 1):
-            for j in range(1, cols + 1):
-                if word1[i - 1] == word2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
+        for i in range(rows):
+            for j in range(cols):
+                if i == 0:
+                    dp[i][j] = j
+                elif j == 0:
+                    dp[i][j] = i
+                elif word1[i - 1] == word2[j -1]:
+                    dp[i][j] = dp[i - 1][j -1]
                 else:
-                    dp[i][j] = min(
-                        dp[i - 1][j],
-                        dp[i][j - 1],
-                        dp[i - 1][j - 1]
-                    ) + 1
-        return dp[rows][cols]
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j -1]) + 1
+        return dp[rows - 1][cols - 1]
