bf3bfede7b64582a91132be5dc426763ec013761
Samuel Suther David <suther.david.samuel@gmail.com>
2025-10-06 14:18:15 -0400

    Time: 97 ms (18.06%), Space: 13 MB (16.3%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -5,38 +5,61 @@ class Solution(object):
         :rtype: int
         """
         n = len(grid)
-        low = grid[0][0]
-        high = n * n - 1
-        ans = n * n - 1
-        self.n = n
-        self.grid = grid
-        
-        while low <= high:
-            t = (low + high) // 2
-            if self.canReachBottomRight(t):
-                ans = t
-                high = t - 1
-            else:
-                low = t + 1
-        return ans
-    
-    def canReachBottomRight(self, t):
-
+        pq = []
+        heapq.heapify(pq)
+        heapq.heappush(pq, (grid[0][0], 0, 0))
         visited = set()
-        mQueue = [(0, 0)]
 
-        while mQueue:
-            r, c = mQueue.pop(0)
+        while pq:
+            h, r, c = heapq.heappop(pq)
             if (r, c) in visited:
                 continue
-            if r == self.n - 1 and c == self.n -1:
-                return True
-            
+            if r == n - 1 and c == n - 1:
+                return h
             visited.add((r, c))
-            for x, y in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
                 nr, nc = r + x, c + y
-                n = self.n
-                if nr >= 0 and nr < n and nc >= 0 and nc < n and self.grid[nr][nc] <= t:
-                    mQueue.append((nr, nc))
-        return False
+                if nr >= 0 and nr < n and nc >= 0 and nc < n:
+                    max_elv = max(h, grid[nr][nc])
+                    heapq.heappush(pq, (max_elv, nr, nc))
+                
+        return n - 1
+
+
+
+    #     n = len(grid)
+    #     low = grid[0][0]
+    #     high = n * n - 1
+    #     ans = n * n - 1
+    #     self.n = n
+    #     self.grid = grid
+        
+    #     while low <= high:
+    #         t = (low + high) // 2
+    #         if self.canReachBottomRight(t):
+    #             ans = t
+    #             high = t - 1
+    #         else:
+    #             low = t + 1
+    #     return ans
+    
+    # def canReachBottomRight(self, t):
+
+    #     visited = set()
+    #     mQueue = [(0, 0)]
+
+    #     while mQueue:
+    #         r, c = mQueue.pop(0)
+    #         if (r, c) in visited:
+    #             continue
+    #         if r == self.n - 1 and c == self.n -1:
+    #             return True
+            
+    #         visited.add((r, c))
+    #         for x, y in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+    #             nr, nc = r + x, c + y
+    #             n = self.n
+    #             if nr >= 0 and nr < n and nc >= 0 and nc < n and self.grid[nr][nc] <= t:
+    #                 mQueue.append((nr, nc))
+    #     return False
             
