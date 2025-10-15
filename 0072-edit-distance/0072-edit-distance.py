e93d1dfe5d7ebadeb66f611723c1edfa42a03830
Samuel Suther David <suther.david.samuel@gmail.com>
2025-10-15 12:07:20 +0800

    Time: 98 ms (17.94%), Space: 15.8 MB (31.9%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -5,18 +5,19 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
-        rows = len(word1) + 1
-        cols = len(word2) + 1
-        dp = [[0] * cols for _ in range(rows)]
+        rows = len(word1)
+        cols = len(word2)
 
-        for i in range(rows):
-            for j in range(cols):
+        dp = [[0 for _ in range(cols + 1)] for _ in range(rows + 1)]
+
+        for i in range(rows + 1):
+            for j in range(cols + 1):
                 if i == 0:
                     dp[i][j] = j
                 elif j == 0:
                     dp[i][j] = i
-                elif word1[i - 1] == word2[j -1]:
-                    dp[i][j] = dp[i - 1][j -1]
+                elif word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j -1]) + 1
-        return dp[rows - 1][cols - 1]
+                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+        return dp[rows][cols]
