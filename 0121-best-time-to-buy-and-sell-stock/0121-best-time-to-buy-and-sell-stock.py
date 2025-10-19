b00ca659acc0badaa4b40c22bf6acc9a113dca8b
Samuel Suther David <suther.david.samuel@gmail.com>
2025-10-19 09:30:04 +0800

    Time: 87 ms (51.46%), Space: 19.6 MB (11.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,8 +5,10 @@ class Solution(object):
         :rtype: int
         """
         maxProfit = 0
-        buy = prices[0]
+        lowest = prices[0]
         for i in range(1, len(prices)):
-            maxProfit = max(maxProfit, prices[i] -  buy)
-            buy = min(buy, prices[i])
+            maxProfit = max(maxProfit, prices[i] - lowest)
+            if prices[i] < lowest:
+                lowest = prices[i]
         return maxProfit
+        
