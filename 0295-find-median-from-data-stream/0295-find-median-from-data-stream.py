2716349d5013c45114ae2cdc6ff0bbdd30d0307c
Samuel Suther David <suther.david.samuel@gmail.com>
2025-10-30 07:52:12 +0800

    Time: 518 ms (87.34%), Space: 36.1 MB (6.41%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,10 +1,11 @@
 class MedianFinder(object):
 
     def __init__(self):
-        self.minHeap = []
         self.maxHeap = []
-        heapq.heapify(self.minHeap)
+        self.minHeap = []
         heapq.heapify(self.maxHeap)
+        heapq.heapify(self.minHeap)
+
         
 
     def addNum(self, num):
@@ -12,36 +13,38 @@ class MedianFinder(object):
         :type num: int
         :rtype: None
         """
-        # we add it first and then we rebalance the heaps
         if len(self.minHeap) == 0:
             heapq.heappush(self.minHeap, num)
-        elif num > self.minHeap[0]:
+        elif num >= self.minHeap[0]:
             heapq.heappush(self.minHeap, num)
         else:
             heapq.heappush(self.maxHeap, num * -1)
-        
-        # rebalancing
-        while abs(len(self.minHeap) - len(self.maxHeap)) >= 2:
-            if len(self.minHeap) > len(self.maxHeap):
-                val = heapq.heappop(self.minHeap)
-                heapq.heappush(self.maxHeap, val * -1)
-            else:
-                val = heapq.heappop(self.maxHeap)
-                heapq.heappush(self.minHeap, val * -1)
+
+        # rebalance
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            top = heapq.heappop(self.minHeap)
+            heapq.heappush(self.maxHeap, top * - 1)
+
+        elif len(self.maxHeap) > len(self.minHeap) + 1:
+            top = heapq.heappop(self.maxHeap) * -1
+            heapq.heappush(self.minHeap, top)
+
 
     def findMedian(self):
         """
         :rtype: float
         """
-        # print("This is the maxHeap: " + str(self.maxHeap))
-        # print("This is the minHeap: " + str(self.minHeap))
+
+        # print("MinHeap: " + str(self.minHeap) + " maxHeap: " + str(self.maxHeap))
+        if len(self.minHeap) == len(self.maxHeap):
+            
+            return (self.minHeap[0] + self.maxHeap[0] * -1) / 2.0
+        
         if len(self.minHeap) > len(self.maxHeap):
             return self.minHeap[0]
-        elif len(self.minHeap) < len(self.maxHeap):
-            return self.maxHeap[0] * -1
         else:
-            # print("it is here")
-            return (self.minHeap[0] + self.maxHeap[0] * -1) / 2.0
+            return self.maxHeap[0] * -1
+
         
 
 
