350dfa60393be7a71ecfbd7eb780d00052de89e4
Samuel Suther David <suther.david.samuel@gmail.com>
2025-11-04 00:55:51 +0800

    Time: 57 ms (89.95%), Space: 26.4 MB (62.73%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
@@ -5,16 +5,12 @@ class Solution:
         prevIdx = 0
         for i in range(1, len(colors)):
             if colors[i] == prevColor:
-                # we need to tie break between them
-                # print("Color at idx: " + str(i) + " same as idx: " + str(prevIdx))
                 if neededTime[i] > neededTime[prevIdx]:
                     totalTime += neededTime[prevIdx]
-                    # print("We add this time: " + str(neededTime[prevIdx]))
                     prevIdx = i
 
                 else:
                     totalTime += neededTime[i]
-                    print("We add this time: " + str(neededTime[i]))
             else:
                 prevColor = colors[i]
                 prevIdx = i
